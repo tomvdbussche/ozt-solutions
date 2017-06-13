62b7d1a34af25b35d13b5cd5fe5db277eaf859cf
Tom Vandenbussche <tom.vandenbussche.s7171@student.hogent.be>
2017-06-13 18:42:29 +0200

    oef 4.5 opgelost

---
+++ oef4.5.R
@@ -0,0 +1,26 @@
+data <- c(400, 350, 400, 500, 300, 350, 200, 500, 200, 250, 250, 500, 350, 100)
+
+m <- 300            # gemiddelde
+n <- 14             # steekproefgrootte
+sm <- mean(data)    # steekproefgemiddelde
+ss <- sd(data)      # steekproefstandaardafwijking
+ssd <- ss / sqrt(n) # steekproefstandaardfout
+a <- 0.05
+
+# H0 = P(m = 300)
+# H1 = P(m > 300)
+
+# Gebruik t-toets (n < 30)
+
+t <- qt(1 - a, df = n -1)
+g <- m + t * ss / sqrt(n)
+
+if (sm < g) {
+  print("accept H0")
+} else {
+  print("reject H0")
+}
+
+# Alternatief
+
+t.test(data, alternative = "greater", mu = m)
