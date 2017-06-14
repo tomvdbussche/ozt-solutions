50d52526c26b4874c68bc1224b55557b36bccc93
Tom Vandenbussche <tom.vandenbussche.s7171@student.hogent.be>
2017-06-14 17:21:29 +0200

    oefening 5.1 & 6.4

---
+++ oef5.1.R
@@ -0,0 +1,40 @@
+data <- read.csv("data/MuziekWijn.csv")
+
+t <- with(data, table(Muziek, Wijn))
+
+# Kruistabel
+
+print(ftable(t))
+
+# Marginalen
+
+print(margin.table(t, 1)) # Muziek
+print(margin.table(t, 2)) # Wijn
+
+# Verwachte waarden
+
+e <- as.array(
+  margin.table(t, 1) %*% t(
+    as.array(margin.table(t, 2))
+  ) / margin.table(t)
+) 
+
+# X2
+
+X2 <- sum((t - e)^2 / e)
+
+# CramersV
+
+V <- sqrt(X2 / n * (2 - 1))
+
+writeLines(
+  c(
+    sprintf("X2 = %s", X2),
+    sprintf("V  = %s", V)
+  )
+)
+
+# Alternatief
+library(lsr)
+V <- cramersV(t)
+print(sprintf("V  = %s", V))
