8d71cb58595aea22501299fe422e554bb8f75d22
Tom Vandenbussche <tom.vandenbussche.s7171@student.hogent.be>
2017-06-15 10:33:56 +0200

    removed incomplete

--- oef6.4.R
+++
@@ -1,9 +0,0 @@
-data <- read.csv("data/bestat-vl-ages2.csv")
-
-t <- with(data, table(Muziek, Wijn))
-
-print(ftable(t))
-
-print(sprintf("Totaal: %f", margin.table(t)))
-print(margin.table(t, 1)) # Muziek
-print(margin.table(t, 2)) # Wijn
