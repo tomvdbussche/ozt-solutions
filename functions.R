8aa7fe9dc319f0858dc26ccb1e888ff79a67acf7
Tom <tom.vandenbussche.s7171@student.hogent.be>
2017-08-21 20:37:31 +0200

    Documentation updated (using roxygen)

--- functions.R
+++ functions.R
@@ -1,29 +1,45 @@
-### Bereken standaardfout
-# sd  = standaardafwijking
-# n   = steekproefgrootte
+# For documentation generation, install the following
+# dnf install libxml2-devel libxslt-devel
+# install.packages("roxygen2")
+
+#' Bereken standaardfout
+#' 
+#' @param sd standaardafwijking
+#' @param n steekproefgrootte
+#' 
+#' @return standaardfout
 se.calc <- function(sd, n) {
   return(sd / sqrt(n))
 }
 
-### Bereken z-score
-# a   = significantieniveau
+#' Bereken z-score
+#' 
+#' @param a significantieniveau
+#' 
+#' @return z-score
 z.calc <- function(a) {
   return (qnorm(a));
 }
 
-### Bereken t-score
-# a   = significantieniveau
-# n   = steekproefgrootte
+#' Bereken t-score
+#' 
+#' @param a significantieniveau
+#' @param n steekproefgrootte
+#' 
+#' @return t-score
 t.calc <- function(a, n) {
   return (qt(a, df = n - 1))
 }
 
-### Bereken kritieke grenswaarde
-# m     = populatiegemiddelde
-# score = z-score / t-score
-# s     = standaardafwijking
-# n     = steekproefgrootte
-# se    = standaardsteekproeffout
+#' Bereken kritieke grenswaarde
+#' 
+#' @param m populatiegemiddelde
+#' @param score = z-score / t-score
+#' @param s standaardafwijking
+#' @param n steekproefgrootte
+#' @param se standaardsteekproeffout
+#' 
+#' @return grenswaarde
 g.calc <- function(m, score, s, n, se = se.calc(s, n)) {
   return(m - score * se)
 }
@@ -34,12 +50,27 @@ g.calc <- function(m, score, s, n, se = se.calc(s, n)) {
 # s   = standaardafwijking
 # n   = steekproefgrootte
 # se  = standaardsteekproeffout
+
+
+#' Bereken overschrijdingskans
+#'
+#' @param x steekproefgemiddelde
+#' @param m populatiegemiddelde
+#' @param s standaardafwijking
+#' @param n steekproefgrootte
+#' @param se standaardsteekproeffout
+#'
+#' @return overschrijdingskans
 p.calc <- function(x, m, s, n, se = se.calc(s, n)) {
   return(pnorm(x, m, se))
 }
 
 ### Test hypothese
 # kijkt of hypothese verworpen moet worden en print het resultaat
+
+#' Test hypothese
+#'
+#' @param reject verwerpingsconditie
 h.test <- function(reject) {
   if (reject) {
     print("reject H0")
